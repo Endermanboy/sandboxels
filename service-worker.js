d3ce3c3ee12e95a0964d8ed4ed5b59a792edc566
Endermanboy <160394416+Endermanboy@users.noreply.github.com>
2026-04-13 19:51:24 +0300

    Prevent caching of favicon and manifest requests
    Add logic to prevent caching of favicon and manifest requests.

--- service-worker.js
+++ service-worker.js
@@ -2,6 +2,12 @@ self.addEventListener('fetch', function(event) {
     if(!event.request.url.startsWith('http')){
       return
     }
+    
+    // Don't cache favicon or manifest requests
+    if (event.request.url.includes('favicon') || event.request.url.includes('manifest.json')) {
+      return;
+    }
+    
     event.respondWith(async function() {
        try{
          var res = await fetch(event.request);
